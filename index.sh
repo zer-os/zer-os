3a20a285ec1e650c6f38aeef195035b2381ff7a9
sam <aquaticcalf@proton.me>
2025-11-28 14:11:21 +0530

    password is in memory

---
+++ index.sh
@@ -0,0 +1,13 @@
+echo ""
+echo ""
+echo "so you installed arch?"
+echo "it's time to make it nice"
+echo ""
+echo "let's begin from the zeros"
+echo ""
+read -s -p "enter your sudo password : " ZEROSPASSWORD
+echo ""
+echo ""
+echo "$ZEROSPASSWORD" | sudo -S ls
+echo ""
+echo ""
